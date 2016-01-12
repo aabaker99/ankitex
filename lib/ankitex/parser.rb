97959b31369449f82ad1727e1d26d40d0431bc1b
Aaron <aabaker99@gmail.com>
2016-01-11 19:42:10 -0600

    updated parser to be slightly less naive: now capable of handling LaTeX recursive curly-braces

--- lib/ankitex/parser.rb
+++ lib/ankitex/parser.rb
@@ -1,26 +1,85 @@
 module Ankitex
+  # Default parser for LaTeX 
   class Parser
-    # Parse a tex file by treating the non-blank line after a \begin{document}
-    #   declaration as the card front and any subsequent lines as the card back
-    # @todo change to parse one or more "theorem" commands
-    #   other styles of latex formatting just need to implement this function
-    #   which returns a list of objects { :front => {front}, :back => {back} }
-    def self.parse_tex(tex_path)
-      rv = nil
-      obj = { :front => "", :back => "" }
-      re = %r{\\begin\{document\}}
+    
+    # parser state enum
+    NULL = 0
+    TITLE = 1
+    TITLE_DONE = 2
+    TEXT = 3
+    TEXT_DONE = 4
+
+    # This parser constructs flashcard objects from LaTeX strings of the form
+    #   \theorem{title}{
+    #     theorem-text
+    #     ...
+    #   }
+    # @return [Array<Hash>] list of flashcard objects with keys
+    #   :front [String] Anki flashcard front (theorem title)
+    #   :back [String] Anki flashcard back (theorem-text)
+    def parse_tex(tex_path)
+      rv = []
       File.open(tex_path) { |fh|
-        lines = fh.readlines.map { |line| line.strip }
-        begin_doc_ind = lines.find_index { |line|
-          re.match(line)
+        contents = fh.read()
+        invocations = contents.split("\\theorem")
+        invocations[1..-1].each { |invocation|
+          obj = { :front => "", :back => "" }
+          depth = 0
+          state = NULL
+          invocation.each_char { |ch|
+            case state
+            when NULL
+              if(ch == "{")
+                state = TITLE
+                depth += 1
+              end
+            when TITLE
+              obj[:front], state, depth = append_section(obj[:front], ch, state, depth)
+              if(state == NULL)
+                state = TITLE_DONE
+              end
+            when TITLE_DONE
+              if(ch == "{")
+                state = TEXT
+                depth += 1
+              end
+            when TEXT
+              obj[:back], state, depth = append_section(obj[:back], ch, state, depth)
+              if(state == NULL)
+                state = TEXT_DONE
+              end
+            when TEXT_DONE
+              break
+            else
+              raise "Invalid state"
+            end
+          }
+          if(obj[:front].empty? or obj[:back].empty?)
+            # incomplete object, perhaps definition of \theorem command
+            $stderr.puts "Incomplete definition at \\theorem invocation #{invocation.inspect}"
+          else
+            rv << obj
+          end
         }
-        unless(begin_doc_ind.nil?)
-          obj[:front] = lines[begin_doc_ind+1]
-          obj[:back] = lines[begin_doc_ind+2..-1].join("\n")
-          rv = obj
-        end
       }
       return rv
+    end
+
+    def append_section(str, ch, state, depth)
+      if(ch == "{")
+        depth += 1
+        str << ch
+      elsif(ch == "}")
+        depth -= 1
+        if(depth == 0)
+          state = NULL
+        else
+          str << ch
+        end
+      else
+        str << ch
+      end
+      return [str, state, depth]
     end
     
     # To import plain text files into Anki:
